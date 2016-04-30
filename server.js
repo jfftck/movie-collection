3579c9822d120d4aacf67ad83519c951b46c42c2
Jeffrey Tackett <Jeffrey Tackett>
2016-04-30 10:25:55 -0700

    Fixed port number.

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.post('/favorites', function(req, res){
 });
 
 // Start the server on port 3000.
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Listening on port 3000");
 });
